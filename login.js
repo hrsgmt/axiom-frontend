ec90522a0a2f95f8126a875ed45b9d943e1297f1
hrsgmt <hrsgmt@gmail.com>
2026-02-08 23:24:34 +0530

    debug what frontend sends

--- login.js
+++ login.js
@@ -1,6 +1,9 @@
+alert("LOGIN JS LOADED");
 async function login(){
   const email = document.getElementById("email").value;
+alert("EMAIL="+email);
   const password = document.getElementById("password").value;
+alert("PASS="+password);
 
   const res = await fetch("https://axiom-backend-final.onrender.com/api/auth/login",{
     method:"POST",
