64b806b20c747560a90eb3844c591c0ecc9def0c
hrsgmt <hrsgmt@gmail.com>
2026-02-09 15:43:32 +0530

    wire posts frontend

---
+++ posts/create.js
@@ -0,0 +1,23 @@
+const API = "https://axiom-backend-v1.onrender.com";
+
+document.getElementById("send").onclick = async () => {
+  const content = document.getElementById("content").value;
+
+  const res = await fetch(API + "/api/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("token")
+    },
+    body: JSON.stringify({ content })
+  });
+
+  const data = await res.json();
+
+  if (data.post) {
+    alert("Posted!");
+    location.href = "/posts/list.html";
+  } else {
+    alert("Error");
+  }
+};
---
+++ posts/list.js
@@ -0,0 +1,12 @@
+const API = "https://axiom-backend-v1.onrender.com";
+
+fetch(API + "/api/posts", {
+  headers: {
+    Authorization: "Bearer " + localStorage.getItem("token")
+  }
+})
+.then(r => r.json())
+.then(d => {
+  document.getElementById("out").innerText =
+    JSON.stringify(d, null, 2);
+});
